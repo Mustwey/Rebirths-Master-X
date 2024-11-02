2c5d7b69adc8ff48ba337ff3cfe6828d99785636
Mustwey <135557972+Mustwey@users.noreply.github.com>
2024-11-02 14:30:21 -0400

    Create AutoTap.lua

---
+++ AutoTap.lua
@@ -0,0 +1,26 @@
+local rs = game:GetService("ReplicatedStorage")
+local tr = rs:FindFirstChild("TappingRemote")
+
+local tap = tr and tr:FindFirstChild("Tap")
+local superTap = tr and tr:FindFirstChild("SuperTap")
+
+getgenv().flags = {
+    autoTap = true,
+    autoSuperTap = true
+}
+
+getgenv().coroutines = getgenv().coroutines or {}
+
+local function createTapCoroutine(remote, flag)
+    return coroutine.create(function()
+        while getgenv().flags[flag] do
+            remote:FireServer()
+            wait(0.01)
+        end
+    end)
+end
+
+if tap and tap:IsA("RemoteEvent") and superTap and superTap:IsA("RemoteEvent") then
+    getgenv().coroutines.autoTap = coroutine.resume(createTapCoroutine(tap, "autoTap"))
+    getgenv().coroutines.autoSuperTap = coroutine.resume(createTapCoroutine(superTap, "autoSuperTap"))
+end
