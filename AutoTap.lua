95a7aa51ddc628c757b50c5d3ae751af2c634c65
Mustwey <135557972+Mustwey@users.noreply.github.com>
2024-11-02 14:43:21 -0400

    Update AutoTap.lua

--- AutoTap.lua
+++ AutoTap.lua
@@ -1,26 +1,29 @@
+-- Initializing services and remotes
 local rs = game:GetService("ReplicatedStorage")
 local tr = rs:FindFirstChild("TappingRemote")
 
-local tap = tr and tr:FindFirstChild("Tap")
-local superTap = tr and tr:FindFirstChild("SuperTap")
+local tapEvent = tr and tr:FindFirstChild("Tap")
+local superTapEvent = tr and tr:FindFirstChild("SuperTap")
 
 getgenv().flags = {
     autoTap = true,
     autoSuperTap = true
 }
 
+local tapDelay = 0.01
+
 getgenv().coroutines = getgenv().coroutines or {}
 
-local function tapDaddy(remote, flag)
+local function tapDaddy(remote, flag, delay)
     return coroutine.create(function()
         while getgenv().flags[flag] do
             remote:FireServer()
-            wait(0.01)
+            wait(delay)
         end
     end)
 end
 
-if tap and tap:IsA("RemoteEvent") and superTap and superTap:IsA("RemoteEvent") then
-    getgenv().coroutines.autoTap = coroutine.resume(tapDaddy(tap, "autoTap"))
-    getgenv().coroutines.autoSuperTap = coroutine.resume(tapDaddy(superTap, "autoSuperTap"))
+if tapEvent and tapEvent:IsA("RemoteEvent") and superTapEvent and superTapEvent:IsA("RemoteEvent") then
+    getgenv().coroutines.autoTap = coroutine.resume(tapDaddy(tapEvent, "autoTap", tapDelay))
+    getgenv().coroutines.autoSuperTap = coroutine.resume(tapDaddy(superTapEvent, "autoSuperTap", tapDelay))
 end
